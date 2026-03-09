03f1b22e018f5954fa556056580113a2b96660fb
sakshi kadu <sakshikadu284@gmail.com>
2026-03-09 15:06:00 +0530

    Fix: dotenv in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
-import dotenv from 'dotenv';
-dotenv.config();
-// import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
 import schemeRoutes from './routes/scheme.js';
 import profileRoutes from './routes/profile.js';
